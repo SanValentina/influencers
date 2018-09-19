b22eecc5c6db0c24dc7e844f8deda663b93016a2
Valentina Antollini <antolliniv@hotmail.com>
2018-09-19 11:13:31 +0100

    added temporary seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+influencer = Influencer.create(name: "Anna Griffin", social_media:"anna_griffin_123", engagement: 6.84)
+influencer.save!
+influencer = Influencer.create(name: "Melissa Peterson", social_media:"melissa_peterson",  engagement: 6.84)
+influencer.save!
+influencer = Influencer.create(name: "Kathryn Smith", social_media:"anna_griffin_123", engagement: 6.84)
+influencer.save!
+influencer = Influencer.create(name: "Patricia Rios", social_media:"patricia_rios_1993", engagement: 6.84)
+influencer.save!
+
+influencer = Influencer.create(name: "Kathryn Smith", social_media:"j_robertson_lol", engagement: 6.84)
+influencer.save!
+
+influencer = Influencer.create(name: "Kathryn Smith", social_media:"sharon_hopkins234", engagement: 6.84)
+influencer.save!
+
+influencer = Influencer.create(name: "Kathryn Smith", social_media:"hross64346", engagement: 6.84)
+influencer.save!
+
+influencer = Influencer.create(name: "Kathryn Smith", social_media:"emwhite", engagement: 6.84)
+influencer.save!
+
+influencer = Influencer.create(name: "Kathryn Smith", social_media:"olivia_wagner_official", engagement: 6.84)
+influencer.save!
